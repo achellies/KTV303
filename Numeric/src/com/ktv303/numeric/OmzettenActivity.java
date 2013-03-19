46cab11c928eaafae4d667d30913e48c844b7d60
Joey <hackrunes@live.nl>
2013-03-19 10:19:44 +0100

    1.2
    - Added omzetspel-screen
    - Updated imageButton omzetten
    - Added activityswitch

---
+++ Numeric/src/com/ktv303/numeric/OmzettenActivity.java
@@ -0,0 +1,16 @@
+package com.ktv303.numeric;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OmzettenActivity extends Activity
+{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_omzetten);
+    }
+    
+}
