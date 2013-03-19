f8ae5db109f3470e43c1b3616f6e07237fb7f915
Joey <hackrunes@live.nl>
2013-03-19 10:35:09 +0100

    1.3
    - Added terug button

--- Numeric/src/com/ktv303/numeric/OmzettenActivity.java
+++ Numeric/src/com/ktv303/numeric/OmzettenActivity.java
@@ -2,15 +2,37 @@ package com.ktv303.numeric;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
 public class OmzettenActivity extends Activity
 {
-
+	ImageButton imageButtonGoBack;
+	
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_omzetten);
+        
+        addListenerOnButton();
     }
+    
+    public void addListenerOnButton()
+    {
+    	 
+		imageButtonGoBack = (ImageButton) findViewById(R.id.imageButtonGoBack);
+ 
+		imageButtonGoBack.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				finish();
+			}
+			
+		});
+ 
+	}
     
 }
