4aee7fd1aff559a9cbfdd62c3f815a867c13dc79
Kevin Schulz <kevin.schulz@hva.nl>
2013-04-22 18:05:48 +0200

    1.9
    Added 4 imageButtons and made actionlisteners for these buttons

--- Numeric/src/com/ktv303/numeric/OmzettenActivity.java
+++ Numeric/src/com/ktv303/numeric/OmzettenActivity.java
@@ -9,6 +9,10 @@ import android.widget.ImageButton;
 public class OmzettenActivity extends Activity
 {
 	private ImageButton imageButtonGoBack;
+	private ImageButton imageButtonDoor1;
+	private ImageButton imageButtonDoor2;
+	private ImageButton imageButtonDoor3;
+	private ImageButton imageButtonDoor4;
 	
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -16,6 +20,9 @@ public class OmzettenActivity extends Activity
         super.onCreate(savedInstanceState);
         
         setContentView( R.layout.activity_omzetten );
+        
+        //Hier moeten de goeie en de foute antwoorden allebij op visible(0) gezet worden.
+        //in de code hieronder worden een van de 2 visible gemaakt afhankelijk van het antwoord.
         
         addListenerOnButton();
     }
@@ -33,6 +40,51 @@ public class OmzettenActivity extends Activity
 				finish();
 			}
 			
+		});
+		
+		
+		//imageButtonDoor1 = (ImageButton) findViewById(R.id.imageButtonDoor1);
+		
+		imageButtonDoor1.setOnClickListener(new OnClickListener()
+		{
+			@Override
+			public void onClick(View arg0)
+			{
+				//Hierin switchen we tussen het dichte deurtje en het open deurtje
+			}
+		});
+		
+		//imageButtonDoor2 = (ImageButton) findViewById(R.id.imageButtonDoor2);
+		
+		imageButtonDoor2.setOnClickListener(new OnClickListener()
+		{
+			@Override
+			public void onClick(View arg0)
+			{
+				//Hierin switchen we tussen het dichte deurtje en het open deurtje
+			}
+		});
+		
+		//imageButtonDoor3 = (ImageButton) findViewById(R.id.imageButtonDoor3);
+		
+		imageButtonDoor3.setOnClickListener(new OnClickListener()
+		{
+			@Override
+			public void onClick(View arg0)
+			{
+				//Hierin switchen we tussen het dichte deurtje en het open deurtje
+			}
+		});
+		
+		//imageButtonDoor4 = (ImageButton) findViewById(R.id.imageButtonDoor4);
+		
+		imageButtonDoor4.setOnClickListener(new OnClickListener()
+		{
+			@Override
+			public void onClick(View agr0)
+			{
+				//Hierin switchen we tussen het dichte deurtje en het open deurtje
+			}
 		});
  
 	}
