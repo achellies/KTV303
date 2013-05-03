a07cb7e3884cee573d8860a553be7eaf24fe42ac
ReckeDJ <hackrunes@live.nl>
2013-05-03 17:12:16 +0200

    1.13
    - Changed the way of drag and drop numbers
    - Added check on answers

--- Numeric/src/com/ktv303/numeric/OpVolgordeActivity.java
+++ Numeric/src/com/ktv303/numeric/OpVolgordeActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import model.Number;
@@ -42,29 +43,32 @@ public class OpVolgordeActivity extends Activity
         this.romansTypeFace = Typeface.createFromAsset( getAssets(), "fonts/font.ttf" );
         
         //  number 1
-        Point point1 = new Point( 80, 170 );
-        this.numbers[0] = new Number( 1, this, this.romansTypeFace, point1 );
+        Point number1StartPoint = new Point( 80, 170 );
+        Point number1GoodPoint = new Point( 352, 279 );
+        this.numbers[0] = new Number( 1, this, this.romansTypeFace, number1StartPoint, number1GoodPoint );
         this.numbers[0].setText( "I" );
         
         //  number 2
-        Point point2 = new Point( 80, 250 );
-        this.numbers[1] = new Number( 2, this, this.romansTypeFace, point2 );
+        Point number2StartPoint = new Point( 160, 170 );
+        Point number2GoodPoint = new Point( 462, 228 );
+        this.numbers[1] = new Number( 2, this, this.romansTypeFace, number2StartPoint, number2GoodPoint );
         this.numbers[1].setText( "V" );
         
         //  number 3
-        Point point3 = new Point( 160, 170 );
-        this.numbers[2] = new Number( 3, this, this.romansTypeFace, point3 );
+        Point number3StartPoint = new Point( 80, 250 );
+        Point number3GoodPoint = new Point( 574, 179 );
+        this.numbers[2] = new Number( 3, this, this.romansTypeFace, number3StartPoint, number3GoodPoint );
         this.numbers[2].setText( "X" );
         
         //  number 4
-        Point point4 = new Point( 160, 250 );
-        this.numbers[3] = new Number( 4, this, this.romansTypeFace, point4 );
+        Point number4StartPoint = new Point( 160, 250 );
+        Point number4GoodPoint = new Point( 690, 125 );
+        this.numbers[3] = new Number( 4, this, this.romansTypeFace, number4StartPoint, number4GoodPoint );
         this.numbers[3].setText( "L" );
         
         for( Number number : this.numbers )
         {
-        	number.refresh();
-        	this.dragLayout.addView( number );
+        	this.dragLayout.addView( number, number.getRLP() );
         }
         
         addListenerOnButton();
@@ -105,7 +109,7 @@ public class OpVolgordeActivity extends Activity
             	for( Number number : this.numbers )
             	{
             		// check all the bounds of the number (square)
-            		if( X > number.getPaddingLeft() && X < number.getPaddingLeft() + 50 && Y > number.getPaddingTop() && Y < number.getPaddingTop() + 50 )
+            		if( X > number.getMarginLeft() && X < number.getMarginLeft() + number.getMeasuredWidth() && Y > number.getMarginTop() && Y < number.getMarginTop() + number.getMeasuredHeight() && number.isFocusable() )
             		{
                     	this.numberID = number.getID();
                     	Log.v( "setted numberID:", String.valueOf( this.numberID ) );
@@ -114,17 +118,27 @@ public class OpVolgordeActivity extends Activity
                   }
         		break;
         	case MotionEvent.ACTION_MOVE:
-        		Log.v( "MOVE", "MOVE" );
+        		Log.v( "MOVE", "X: " + X + " Y: " + Y );
         		if( this.numberID > 0 )
         		{
-                	this.numbers[ this.numberID - 1 ].setPaddingLeft( X - 25 );
-                	this.numbers[ this.numberID - 1 ].setPaddingTop( Y - 25 );
-        			this.numbers[ this.numberID - 1 ].refresh();
+                	this.numbers[ this.numberID - 1 ].moveNumber( X - 25, Y - 25 );
                 }
         		break;
         	case MotionEvent.ACTION_UP: 
         		// touch drop - just do things here after dropping
-        		
+        		if( this.numberID > 0 )
+        		{
+        			if( this.numberID == 1 )
+        			{
+        				this.numbers[0].checkAnswer( X, Y );
+        			} else if( this.numberID == 2 ) {
+        				this.numbers[1].checkAnswer( X, Y );
+        			} else if( this.numberID == 3 ) {
+        				this.numbers[2].checkAnswer( X, Y );
+        			} else if( this.numberID == 4 ) {
+        				this.numbers[3].checkAnswer( X, Y );
+        			}
+        		}
         		break;
         }
         
--- Numeric/src/model/Number.java
+++ Numeric/src/model/Number.java
@@ -5,11 +5,17 @@ import com.ktv303.numeric.R;
 import android.content.Context;
 import android.graphics.Point;
 import android.graphics.Typeface;
+import android.view.Gravity;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class Number extends TextView {
-	private int paddingLeft;
-	private int paddingTop;
+	private RelativeLayout.LayoutParams RLP;
+	private int originalX;
+	private int originalY;
+	private int goodX;
+	private int goodY;
 	private int id;
 	
 	public Number( int numberId, Context content, Typeface font )
@@ -21,37 +27,48 @@ public class Number extends TextView {
 		//  set text size
 		super.setTextSize( 50 );
 		//  set text color
-		super.setTextColor( getResources().getColor( R.color.Black ) );
+		super.setTextColor( getResources().getColor( R.color.White ) );
+		//  set background color
+		super.setBackgroundColor( getResources().getColor( R.color.Grey ) );
+		//  set height of number
+		super.setHeight( 50 );
+		//  set width of number
+		super.setWidth( 50 );
+		//  set horizontal and vertical alignment
+		super.setGravity( Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL );
+		//  set padding top
+		super.setPadding( 0, 5, 0, 0 );
+		
+		super.setFocusable( true );
 		
 		this.id = numberId;
 	}
 
-	public Number( int numberId, Context content, Typeface font, Point point )
+	public Number( int numberId, Context content, Typeface font, Point startPoint, Point goodPoint )
 	{
 		this( numberId, content, font );
 		
-		this.paddingLeft = point.x;
-		this.paddingTop = point.y;
+		this.RLP = new RelativeLayout.LayoutParams( LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT );
+		this.originalX = this.RLP.leftMargin = startPoint.x;
+		this.originalY = this.RLP.topMargin = startPoint.y;
+		this.goodX = goodPoint.x;
+		this.goodY = goodPoint.y;
 	}
 	
-	public void setPaddingLeft( int newPaddingLeft )
+	public void moveNumber( int X, int Y )
 	{
-		this.paddingLeft = newPaddingLeft;
+		this.RLP.leftMargin = X;
+		this.RLP.topMargin = Y;
 	}
 	
-	public int getPaddingLeft()
+	public int getMarginLeft()
 	{
-		return this.paddingLeft;
+		return this.RLP.leftMargin;
 	}
 	
-	public void setPaddingTop( int newPaddingTop )
+	public int getMarginTop()
 	{
-		this.paddingTop = newPaddingTop;
-	}
-	
-	public int getPaddingTop()
-	{
-		return this.paddingTop;
+		return this.RLP.topMargin;
 	}
 	
 	public int getID()
@@ -59,9 +76,39 @@ public class Number extends TextView {
 		return this.id;
 	}
 	
-	public void refresh()
+	private void good()
 	{
-		this.setPadding( this.paddingLeft, this.paddingTop, 0, 0 );
+		this.setBackgroundColor( getResources().getColor( R.color.Green ) );
+		this.moveNumber( this.goodX - 25, this.goodY - 25 );
+		this.setFocusable( false );
+	}
+	
+	private void wrong()
+	{
+		this.setBackgroundColor( getResources().getColor( R.color.Red ) );
+		this.resetPosition();
+	}
+	
+	public void checkAnswer( int X, int Y )
+	{
+		if( ( ( this.goodX - 15 ) < X && X < ( this.goodX + 15 ) ) && ( ( this.goodY - 15 ) < Y && Y < ( this.goodY + 15 ) ) )
+		{
+			//  correct answer was given
+			this.good();
+		} else {
+			//  wrong answer was given, reset position
+			this.wrong();
+		}
+	}
+	
+	public void resetPosition()
+	{
+		this.moveNumber( this.originalX, this.originalY );
+	}
+	
+	public RelativeLayout.LayoutParams getRLP()
+	{
+		return this.RLP;
 	}
 	
 }
