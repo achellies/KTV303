06757b27385c72513a5708ebe74fde1bb9534f79
Josh <joshua_onwezen@Hotmail.com>
2013-05-12 21:47:05 +0200

    1.36
    Van de imageviews textviews gemaakt en onclicks half ingevuld

--- Numeric/src/com/ktv303/numeric/LoginActivity.java
+++ Numeric/src/com/ktv303/numeric/LoginActivity.java
@@ -1,17 +1,28 @@
 package com.ktv303.numeric;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.RelativeLayout.LayoutParams;
 
 public class LoginActivity extends Activity {
 	private ImageButton imageButtonGoBack;
-	private ImageButton imageButtonLogin;
-	private ImageButton imageButtonRegister;
 	private ImageButton imageButtonRegisterScreen;
+	private Button		buttonLogin;
+	private Button		buttonRegister;
+	private Typeface	romansTypeFace;
+	
+
+
+	
+	
 	public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
@@ -19,14 +30,20 @@ public class LoginActivity extends Activity {
         setContentView( R.layout.activity_login );
         
         //  set imageButtonGoBack
-        this.imageButtonGoBack = (ImageButton) findViewById( R.id.imageButtonGoBack );
-        //  set imageButtonLogin
-        this.imageButtonLogin = (ImageButton) findViewById( R.id.imageButtonLogin );
-        //  set imageButtonRegister
-        this.imageButtonRegister = (ImageButton) findViewById( R.id.imageButtonRegister );
+        imageButtonGoBack = (ImageButton) findViewById( R.id.imageButtonGoBack );
+        //  set buttonViewLogin
+        buttonLogin = (Button) findViewById( R.id.buttonLogin );
+        //  set buttonRegister
+        buttonRegister = (Button) findViewById( R.id.buttonRegister );
         //  set imageButtonRegisterScreen
-        this.imageButtonRegisterScreen = (ImageButton) findViewById( R.id.imageButtonRegisterScreen );
-
+        imageButtonRegisterScreen = (ImageButton) findViewById( R.id.imageButtonRegisterScreen );
+        
+    	romansTypeFace = Typeface.createFromAsset( getAssets(), "fonts/font.ttf" );
+		buttonLogin.setTypeface(romansTypeFace, Typeface.BOLD);
+		buttonRegister.setTypeface(romansTypeFace, Typeface.BOLD);
+        
+        // set txtViewRegister to invisible
+		buttonRegister.setVisibility(buttonRegister.GONE);
         
         addListenerOnButton();
     }
@@ -42,7 +59,7 @@ public class LoginActivity extends Activity {
 			}
 			});
 		// add click listener to login button
-		imageButtonLogin.setOnClickListener(new OnClickListener()
+		buttonLogin.setOnClickListener(new OnClickListener()
 			{
 			@Override
 			public void onClick(View arg0) {
@@ -50,10 +67,11 @@ public class LoginActivity extends Activity {
 					}
 			});
 		// add click listener to register button
-		imageButtonRegister.setOnClickListener(new OnClickListener()
+		buttonRegister.setOnClickListener(new OnClickListener()
 			{
 			@Override
 			public void onClick(View arg0) {
+				finish();
 					}
 			});
 		// add click listener to register screen button
@@ -61,6 +79,8 @@ public class LoginActivity extends Activity {
 			{
 			@Override
 			public void onClick(View arg0) {
+					buttonLogin.setVisibility(View.GONE);
+					buttonRegister.setVisibility(View.VISIBLE);
 					}
 			});
 
