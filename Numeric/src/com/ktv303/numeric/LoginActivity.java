27c17b9a3a460cea13d6ba30f7aedd529806aae5
ReckeDJ <hackrunes@live.nl>
2013-05-13 16:41:36 +0200

    1.39
    - Added loginscript (not ready yet)

--- Numeric/src/com/ktv303/numeric/LoginActivity.java
+++ Numeric/src/com/ktv303/numeric/LoginActivity.java
@@ -1,28 +1,45 @@
 package com.ktv303.numeric;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.Gravity;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.widget.RelativeLayout.LayoutParams;
+import android.widget.Toast;
+
+import model.DatabaseHandler;
+import model.UserFunctions;
 
 public class LoginActivity extends Activity {
-	private ImageButton imageButtonGoBack;
-	private Button 		buttonRegisterScreen;
-	private Button 		buttonLoginScreen;
-	private Button		buttonLogin;
-	private Button		buttonRegister;
-	private Typeface	romansTypeFace;
+	private ImageButton 		imageButtonGoBack;
+	private Button 				buttonRegisterScreen;
+	private Button 				buttonLoginScreen;
+	private Button				buttonLogin;
+	private Button				buttonRegister;
+	private Typeface			romansTypeFace;
+	private EditText			editTextUsername;
+	private EditText			editTextPassword;
+	private ProgressDialog		progressDialog;
+	private String errorMessage;
 	
-
-
-	
+	// JSON Response node names
+	private static String KEY_SUCCESS = "success";
+	private static String KEY_ERROR = "error";
+	private static String KEY_ERROR_MSG = "error_msg";
+	private static String KEY_UID = "uid";
+	private static String KEY_USERNAME = "username";
+	private static String KEY_CREATED_AT = "created_at";
 	
 	public void onCreate(Bundle savedInstanceState)
     {
@@ -40,17 +57,17 @@ public class LoginActivity extends Activity {
         buttonRegisterScreen = (Button) findViewById( R.id.buttonRegisterScreen );
         //  set buttonLoginScreen
         buttonLoginScreen = (Button) findViewById( R.id.buttonLoginScreen );
+        //  set editTextUsername
+        this.editTextUsername = (EditText) findViewById( R.id.editTextUsername );
+        //  set editTextPassword
+        this.editTextPassword = (EditText) findViewById( R.id.editTextPassword );
         
     	romansTypeFace = Typeface.createFromAsset( getAssets(), "fonts/font.ttf" );
 		buttonLogin.setTypeface(romansTypeFace, Typeface.BOLD);
 		buttonRegister.setTypeface(romansTypeFace, Typeface.BOLD);
 		buttonLoginScreen.setTypeface(romansTypeFace, Typeface.BOLD);
 		buttonRegisterScreen.setTypeface(romansTypeFace, Typeface.BOLD);
-        
-        // set txtViewRegister to invisible
-		buttonRegister.setVisibility(buttonRegister.GONE);
-		buttonLoginScreen.setVisibility(buttonLoginScreen.GONE);
-        
+		
         addListenerOnButton();
     }
 	
@@ -66,43 +83,104 @@ public class LoginActivity extends Activity {
 			});
 		// add click listener to login button
 		buttonLogin.setOnClickListener(new OnClickListener()
-			{
+		{
 			@Override
 			public void onClick(View arg0) {
-				finish();
-					}
-			});
+				progressDialog = ProgressDialog.show( LoginActivity.this, "", "Gegevens controleren.", true );
+                new Thread(new Runnable() {
+                	public void run() {
+                		login();                          
+                	}
+                }).start();
+			}
+		});
 		// add click listener to register button
 		buttonRegister.setOnClickListener(new OnClickListener()
-			{
+		{
 			@Override
-			public void onClick(View arg0) {
-				finish();
-					}
-			});
+			public void onClick(View arg0)
+			{
+				
+			}
+		});
 		// add click listener to register screen button
 		buttonLoginScreen.setOnClickListener(new OnClickListener()
-			{
+		{
 			@Override
 			public void onClick(View arg0) {
-					buttonLogin.setVisibility(View.VISIBLE);
-					buttonRegister.setVisibility(View.GONE);
-					buttonLoginScreen.setVisibility(View.GONE);
-					buttonRegisterScreen.setVisibility(View.VISIBLE);
-					
-					}
-			});
+				
+			}
+		});
 		// add click listener to register screen button
 		buttonRegisterScreen.setOnClickListener(new OnClickListener()
-			{
+		{
 			@Override
 			public void onClick(View arg0) {
-					buttonLogin.setVisibility(View.GONE);
-					buttonRegister.setVisibility(View.VISIBLE);
-					buttonLoginScreen.setVisibility(View.VISIBLE);
-					buttonRegisterScreen.setVisibility(View.GONE);
-					}
-			});
+				
+			}
+		});
 
 	}
+	
+	private void login()
+	{
+		String username = this.editTextUsername.getText().toString();
+		String password = this.editTextPassword.getText().toString();
+		UserFunctions userFunction = new UserFunctions();
+		Log.d("Button", "Login");
+		JSONObject json = userFunction.loginUser( username, password );
+		
+		// check for login response
+		try {
+			if( json.getString(KEY_SUCCESS) != null )
+			{
+				String res = json.getString(KEY_SUCCESS); 
+				if(Integer.parseInt(res) == 1){
+					// user successfully logged in
+					// Store user details in SQLite Database
+					DatabaseHandler db = new DatabaseHandler(getApplicationContext());
+					JSONObject json_user = json.getJSONObject("user");
+					
+					// Clear all previous data in database
+					userFunction.logoutUser(getApplicationContext());
+					db.addUser(json_user.getString(KEY_USERNAME), json.getString(KEY_UID), json_user.getString(KEY_CREATED_AT));						
+					
+					// Launch Dashboard Screen
+					//Intent dashboard = new Intent(getApplicationContext(), DashboardActivity.class);
+					
+					// Close all views before launching Dashboard
+					//dashboard.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+					//startActivity(dashboard);
+					
+					// Close Login Screen
+					finish();
+				}else{
+					// Error in login
+					this.errorMessage = "Onjuiste gegevens";
+					showAlert();
+				}
+			}
+		} catch( JSONException e ) {
+			this.progressDialog.dismiss();
+           	Log.v( "Exception", e.getMessage() );
+		}
+	}
+	
+	private void showAlert(){
+        LoginActivity.this.runOnUiThread(new Runnable() {
+            public void run() {
+                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                builder.setTitle("Oeps!");
+                builder.setMessage(errorMessage)
+                       .setCancelable(false)
+                       .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                           public void onClick(DialogInterface dialog, int id) {
+                           }
+                       });                     
+                AlertDialog alert = builder.create();
+                alert.show();
+                progressDialog.dismiss();
+            }
+        });
+    }
 }
---
+++ Numeric/src/model/UserFunctions.java
@@ -0,0 +1,86 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.util.Log;
+
+public class UserFunctions {
+	
+	private JSONParser jsonParser;
+	
+	private static String loginURL = "http://ktv303.ddict.nl/api/";
+	private static String registerURL = "http://ktv303.ddict.nl/api/";
+	
+	private static String login_tag = "login";
+	private static String register_tag = "register";
+	
+	// constructor
+	public UserFunctions(){
+		jsonParser = new JSONParser();
+	}
+	
+	/**
+	 * function make Login Request
+	 * @param username
+	 * @param password
+	 * */
+	public JSONObject loginUser(String username, String password){
+		// Building Parameters
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("tag", login_tag));
+		params.add(new BasicNameValuePair("username", username));
+		params.add(new BasicNameValuePair("password", password));
+		JSONObject json = jsonParser.getJSONFromUrl(loginURL, params);
+		// return json
+		Log.e("JSON", json.toString());
+		return json;
+	}
+	
+	/**
+	 * function make Login Request
+	 * @param username
+	 * @param password
+	 * */
+	public JSONObject registerUser(String username, String password){
+		// Building Parameters
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("tag", register_tag));
+		params.add(new BasicNameValuePair("username", username));
+		params.add(new BasicNameValuePair("password", password));
+		
+		// getting JSON Object
+		JSONObject json = jsonParser.getJSONFromUrl(registerURL, params);
+		// return json
+		return json;
+	}
+	
+	/**
+	 * Function get Login status
+	 * */
+	public boolean isUserLoggedIn(Context context){
+		DatabaseHandler db = new DatabaseHandler(context);
+		int count = db.getRowCount();
+		if(count > 0){
+			// user logged in
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Function to logout user
+	 * Reset Database
+	 * */
+	public boolean logoutUser(Context context){
+		DatabaseHandler db = new DatabaseHandler(context);
+		db.resetTables();
+		return true;
+	}
+	
+}
